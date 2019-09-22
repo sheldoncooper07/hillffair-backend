7b992f09f169c123be7d7742ed8b6ea546ed8040
Moulik <moulikbhardwaj@gmail.com>
2019-09-23 03:45:27 +0530

    Added values.py file for storing variables

---
+++ values.py
@@ -0,0 +1,15 @@
+# --------Values to be used by backend--------------
+# global variables
+
+sqlHost = "52.41.147.246"
+sqlUser = "hillffair"
+sqlPass = "1qaz2wsx"
+dbName = "Hillffair2k19"
+
+#faceSmash module
+
+faceSmash_request = "/facesmash"
+faceSmash_league_diff = 5
+faceSmash_tableName = "facesmashusers"
+
+# TODO add your own variables here instead of hardcoding them
