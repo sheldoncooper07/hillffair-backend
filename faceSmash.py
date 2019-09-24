5d61f94b6f68baaeac4bb3ce28ce1a7ead6cd77a
Moulik <moulikbhardwaj@gmail.com>
2019-09-24 23:46:50 +0530

    changed faceSmash request from argument parameter to form parameter

--- faceSmash.py
+++ faceSmash.py
@@ -52,7 +52,7 @@ def faceSmash(connection):
                         return "[{image_url:"+str(ans[0]["url"])+"},{image_url:"+str(ans[1]["url"])+"}]"
                 # for POST requests
                 elif request.method == "POST":
-                        imgURL = request.args.get('image_url')
+                        imgURL = request.form.get('image_url')
                         # incrementing the rating of winning user
                         cursor.execute("update "+str(faceSmash_tableName)+" set rating = rating + "+str(
                             faceSmash_ratingIncrease)+" where url = '"+str(imgURL)+"';")
