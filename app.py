d6c35c08daac96d9a38b8bb614774eae49227948
utkarshsingh99 <utkarshsingh369@gmail.com>
2019-09-25 22:28:25 +0530

    Feed done, with merge conflicts

--- app.py
+++ app.py
@@ -11,10 +11,6 @@ import faceSmash as faceSmash
 
 global cursor
 
-
-
-
-
 connection = pymysql.connect(host='localhost',
                                          user='hillffair',
                                          password='1qaz2wsx',
@@ -113,13 +109,8 @@ app.add_url_rule('/faceSmash', 'faceSmash.faceSmash', faceSmash.faceSmash, metho
 
 @app.route('/quiz/questions',methods=['POST'])
 def quiz():
-<<<<<<< HEAD
-    category=request.form['category']
-    query="SELECT id,ques,option1,option2,option3,option4 FROM quiz WHERE category="+category+" ORDER BY RAND() LIMIT 10" 
-=======
     category=request.form.get('category')
     query="SELECT id,ques,option1,option2,option3,option4 FROM quiz WHERE category={} ORDER BY RAND() LIMIT 10".format(category)
->>>>>>> fea6762091bf34d21d8ad11bb767c329212c5296
     cursor.execute(query)
     questions=cursor.fetchall()
     return json.dumps(questions)
@@ -210,7 +201,7 @@ def feedg(page_index, firebase_id):
 
     
     
-<<<<<<< HEAD
+
 # @app.route('/rewards',methods=['POST'])
 # # def rewards():
 # #     firebase_id=request.form.firebase_id
@@ -221,23 +212,10 @@ def feedg(page_index, firebase_id):
 
 @app.route('/rewards',methods=['POST'])
 def rewards():
-    firebase_id=request.form.get('firebase_id')
-    candies=request.form['sub_candies']
-    query="UPDATE profile SET points=points-'"+candies+"' WHERE firebase_id='"+firebase_id+"'"
-    cursor.execute(query)
-    connection.commit()
-    return {"status_code":200}# not in docs
-=======
-@app.route('/rewards',methods=['POST'])
-def rewards():
     firebase_id=request.form.firebase_id
     candies=request.form.sub_candies
     query="UPDATE profile SET points=points-'"+candies+"' WHERE firebase_id='"+firebase_id+"'"
     return {"status_code":200}# not in docs
-    
-    
->>>>>>> fea6762091bf34d21d8ad11bb767c329212c5296
-
     #---------------------------------------------------------------------------
     
 
